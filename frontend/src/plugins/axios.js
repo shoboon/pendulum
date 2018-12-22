d1a1945aa69519fbb57696706a12415bdaa76d4c
Nolit <yaboonjapan@gmail.com>
2018-12-22 14:49:27 +0900

    mockのtypoを修正

--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -3,8 +3,8 @@ import MockAdapter from 'axios-mock-adapter'
 export default function({ $axios }) {
     const mock = new MockAdapter($axios, { delayResponse: 500 })
 
-    mock.onGet('/numbered_tikect').reply(200, {
-        numberedTikect: "550e8400e29b41d4a716446655440000"
+    mock.onGet('/numbered_ticket').reply(200, {
+        numberedTicket: "550e8400e29b41d4a716446655440000"
     })
 
     mock.onGet('/waiting_users/550e8400e29b41d4a716446655440000').reply(200, {
